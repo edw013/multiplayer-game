b41d7aec226a3996e4e11de53099b30c28126b14
Ethan <ehwang@salesforce.com>
2018-08-19 19:27:32 -0700

    Example sockets usage

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-var app = require("express")();
-var http = require("http").Server(app);
+// dependencies
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
-app.get("/", function(req, res) {
-    res.send("<h1>Hello world</h1>");
+const port = process.env.port || 3000;
+
+// routing
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
 });
 
-http.listen(3000, function() {
-    console.log("listening on *:3000");
+// socket
+io.on("connection", function(socket){
+  console.log("a user connected");
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit("chat message", msg);
+    });
+});
+
+http.listen(port, function(){
+  console.log("listening on *:", port);
+});
+
+const g = require("./game.js");
